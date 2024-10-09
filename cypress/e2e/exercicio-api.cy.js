59af7b1538e484c602b3a9163394db7656c87190
Zhu Ze Feng <zhusandro@gmail.com>
2024-10-09 10:13:56 -0300

    correcao testes cypress

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -4,7 +4,7 @@ import contratoUsuarios from '../contracts/usuarios.contract'
 describe('Testes da Funcionalidade Usuários', () => {
   let token
   beforeEach(() => {
-    cy.token('beltrano@qa.com', 'teste').then(tkn => { token = tkn })
+    cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
   });
 
   it('Deve validar contrato de usuários', () => {
@@ -79,6 +79,5 @@ describe('Testes da Funcionalidade Usuários', () => {
         })
       })
   });
-
 
 });
